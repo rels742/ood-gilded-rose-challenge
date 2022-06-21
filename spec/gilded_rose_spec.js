4cdec24a50ff2f69549a1a721f9808bdeca922ff
Lorel <lorel.oboue@gmail.com>
2022-06-22 00:47:23 +0100

    first few tests added

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,42 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("standard item: sellIn = 0, quality = 0", function() {
+    const gildedRose = new Shop([ new Item("standard item", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("standard item");
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
   });
+
+  it("standard item: sellIn = 1, quality = 3", function() {
+    const gildedRose = new Shop([ new Item("standard item", 1, 3) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("standard item");
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(2);
+  });
+
+  // see if there are any special cases for a standard item and test them.
+
+  it("standard item: sellIn = 0, quality = 3", function() {
+    const gildedRose = new Shop([ new Item("standard item", 0, 3) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("standard item");
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(1);
+  });
+
+  // write tests for special items, eg aged brie which has 4 possible tests
+  it("aged brie: sellIn = 0, quality = 3", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 0, 3) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("Aged Brie");
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(5);
+  });
+
+
+  // after writing tests for all special items, write tests for mana cake.
 
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,13 +12,20 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+      const item = this.items[i]
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
+          // this code deals with if the quality is greater than 0
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            // ordinary products decrease by 1, if quality is greater than 0
+            console.log(`item: ${item.name} decreasing quality by 1`);
             this.items[i].quality = this.items[i].quality - 1;
-          }
+          } 
+        } else {
+          console.log(`item: ${item.name} the quality is already 0`);
         }
       } else {
+        // this code deals with aged brie or backstage passes
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -35,22 +42,37 @@ class Shop {
           }
         }
       }
+
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        // any item that isn't sufuras sell in, decreases by 1 
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
+
       if (this.items[i].sellIn < 0) {
+        // this code deals with if sell in is below 0 
         if (this.items[i].name != 'Aged Brie') {
+          // not aged brie
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            // not backstage passes 
             if (this.items[i].quality > 0) {
+              // but the quality is over 0
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                // but if it is not sulfuras then the quality decreases by 1 
+                // this applies to a standard item - any item that is not stated above 
+                console.log(`item: ${item.name} decreasing quality by 1 (again as sellin is less than 0)`);
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
+            // if it is backstage passes to the concert then the quality = 0  
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
+          //if it is aged brie
           if (this.items[i].quality < 50) {
+            // if the quality is less than 50 then it increases by 1
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
